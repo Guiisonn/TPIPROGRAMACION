fb07eab07de24aa8683bdb9026078b8c6a6a4340
LilGuiiso <guiisoide@gmail.com>
2025-06-28 16:54:11 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,21 +1,79 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include <string>
+#include <iostream>
+using namespace std;
 
-// Prototipos del menú
 void mostrarMenu();
 void menuReportes();
 
-// Prototipos de carga de lotes
-void cargarMarcas();
-void cargarProductos();
-void cargarFormasPago();
-void cargarVentas();
+//   LOTE       MARCAS     -------------------
 
-// Prototipos de reportes (a implementar después)
+void CargarLoteMarcas (int VcodMarca[10],string VnomMarca[10]);
+
+void CargarCodMarca(int i,int VcodMarca[10]);
+
+
+// LOTE      PRODUCTOS -----------
+
+void CargarNomMarca (int i,string VnomMarca[10]);
+
+void CargarLoteProd (int VcodProd[20],
+                 string VnomProd[20],
+                 float VpreVta[20],
+                 float VpreCmp[20],
+                 int Vstock[20],
+                 int VcodMarcaComp[20],int VcodMarca[10]);
+
+void VerifComp(int i,int VcodMarcaComp[20],int VcodMarca[10]);
+
+//   LOTE     FORMAS    PAGO --------------
+
+void CargarLoteFPago (string VcodFP[5],string VnombFP[5],int Vporcen[5]);
+
+int ValidarCFP(string codFP,string VcodFP[5]);
+
+void RevalidarFP (int indfp,bool&BE,bool&BM,bool&BT,bool&BTC,bool&BC);
+
+
+//  LOTE     PROCESO   --------------------------------
+
+
+void Proceso (int VcodProd[20],string VnombFP[5],int VacuRec [20],int VcantVnd[20],int Vindice[20],int Vstock[20],float VpreVta[20],int Vporcen[5] );
+
+
+int indiceProducto(int codProd,int VcodProd[20]);
+int indiceFormaPago(string formPag,string VnombFP[5]);
+int calcularTotal (int inpr,int infp,int cantVnd,float VpreVta[20],int Vporcen[5]);
+void ordenPuntoA(int VcantVnd[20],int Vindice[20]);
+
 void reporteRecaudacionProducto();
+
 void reportePorcentajeFormasPago();
+
 void reporteVentasPorMarcaYForma();
+
 void reporteProductosSinVenta();
+
 void reporteTop10Clientes();
 
-#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // FUNCIONES_H_INCLUDED
+
